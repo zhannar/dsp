ec1b6238c2323808fa328272312c7a509f1c8515
zhannar <zr28@cornell.edu>
2016-02-03 17:23:06 -0500

    Completed the verbing function in q6_strings

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -133,9 +133,14 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
 
-
+    if len(s) < 3:
+        print s
+    elif s[-3:] == "ing":
+        print s + "ly"
+    else:
+        print s + "ing"
+        
 def not_bad(s):
     """
     Given a string, find the first appearance of the substring 'not'
