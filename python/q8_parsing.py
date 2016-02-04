31ee84c6ae91dc0a1943dd0d935d8f54f81880a3
zhannar <zr28@cornell.edu>
2016-02-04 17:17:55 -0500

    Updated q8_parsing.py which parses the footbal.csv file and returns the team name which has the smallest difference in goals made vs goals allowed.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,44 @@
-#The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+# -*- coding: utf-8 -*-
+"""Line above added to get rid of this error:
+SyntaxError: Non-ASCII character '\xe2' in file q8_parsing.py on line 4, 
+but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
 
-# The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
+Per: http://stackoverflow.com/questions/21639275/python-syntaxerror-non-ascii-character-xe2-in-file
+
+The football.csv file contains the results from the English Premier League. 
+The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
+goals scored for and against each team in that season (so Arsenal scored 79 goals 
+against opponents, and had 36 goals scored against them). 
+
+Write a program to read the file, then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+
+The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
+"""
+
+data = "football.csv"
+
+import pandas as pd
+
+# Step 1: Bring the data in.
+football_df = pd.read_csv(data)
+
+# Step 2: Create a new column which calclates the difference between goals and goals allowed.
+football_df["Goals Difference"] = abs(football_df["Goals"] - football_df["Goals Allowed"])
+
+# Step 3: Identify the minimum difference and pop out the information for it
+
+# This method gives all the information:
+#print football_df.loc[football_df['Goals Difference'].idxmin()]
+
+# This method pulls only the team name:
+print football_df.loc[football_df['Goals Difference'].idxmin()]["Team"]
 
 
-import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+
+
+
+
+
