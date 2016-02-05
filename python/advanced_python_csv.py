2a1054a959dd316b395180685c9cd2aa23505426
zhannar <zr28@cornell.edu>
2016-02-05 14:39:46 -0500

    Created a file which took the list of emails and wrote a csv file based off that list. Adding both files.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+# ----------------------------------------------------------------------
+# Part II - Write to CSV File
+# Q5. Write email addresses from Part I to csv file.
+# ----------------------------------------------------------------------
+
+data = "faculty.csv"
+
+# Import data
+import pandas as pd
+faculty_df = pd.read_csv(data)
+
+# Create a list of the emails.
+email_list = []
+	
+for email in faculty_df[" email"]:
+	email_list.append(email.rstrip())
+
+# Create & open a file for writing
+csvfile = open("emails.csv","w")
+
+# Write the file & close it.
+for email in email_list:	
+	csvfile.write(str(email))
+	csvfile.write("\n")
+
+csvfile.close()
