49c192483956d35721464a9e8ec26ddd7c185872
zhannar <zr28@cornell.edu>
2016-03-19 06:37:21 -0400

    Added folder to put test code while learning pythong & wrote experimental python script to test out gather functionality.

---
+++ test_code/python_gather_experiment.py
@@ -0,0 +1,9 @@
+def my_gather(*args):
+  print "There once was a man from Nantucket, he liked to stand on the street corner and whisper words to people as they passed him. I had a few minutes to wait for the bus, and these are the words I overheard him say that day:"
+
+  for word in args:
+    print word
+
+my_gather("I","cheese","donut","Paris","Istanbul","music","desperation")
+
+
